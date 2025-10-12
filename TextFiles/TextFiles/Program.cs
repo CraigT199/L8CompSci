4767fb71f15d840ca0c050346b6ac517c07dbd36
CraigT199 <craigt@stpaulsschool.org.uk>
2025-10-12 15:31:34 +0100

    Update Program.cs

--- TextFiles/TextFiles/Program.cs
+++ TextFiles/TextFiles/Program.cs
@@ -76,6 +76,52 @@ List<string> AlliterationStations(string fileName)
     return output;
 }
 
+string MostStations(string fileName)
+{
+    Dictionary<string, int> tubeLines = new Dictionary<string, int> { };
+    foreach (var line in File.ReadAllLines(fileName))
+    {
+        string[] sections = line.Split(", ");
+        string tubeLine = sections.Last();
+        if (tubeLines.ContainsKey(tubeLine))
+            tubeLines[tubeLine] += 1;
+        else
+            tubeLines[tubeLine] = 1;
+    }
+    // Stack Overflow
+    return tubeLines.MaxBy(line => line.Value).Key;
+}
+
+Dictionary<string, int> GetWordFrequency(string fileName)
+{
+    Dictionary<string, int> frequencies = new Dictionary<string, int>();
+    foreach (var line in File.ReadAllLines(fileName))
+    {
+        string word = "";
+        foreach (char c in line)
+        {
+            if (char.IsLetter(c))
+                word += char.ToLowerInvariant(c);
+            else if (word.Length > 0)
+            {
+                if (frequencies.ContainsKey(word))
+                    frequencies[word] += 1;
+                else
+                    frequencies[word] = 1;
+                word = "";
+            }
+        }
+        if (word.Length > 0)
+        {
+           if (frequencies.ContainsKey(word))
+                    frequencies[word] += 1;
+            else
+                frequencies[word] = 1; 
+        }
+    }
+    return frequencies.OrderByDescending(frequency => frequency.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+}
+
 // Question 2
 Debug.Assert(FindStation("./stations.txt", "station").SequenceEqual(new List<string> { "Battersea Power Station, Northern" }));
 // Question 3
@@ -84,4 +130,9 @@ Debug.Assert(FindNoSharedLetters("./stations.txt", "piranha").SequenceEqual(new 
 Debug.Assert(FindNoSharedLetters("./stations.txt", "sturgeon").SequenceEqual(new List<string> { "Balham, Northern", "Blackwall, Docklands Light Railway" }));
 Debug.Assert(FindNoSharedLetters("./stations.txt", "bacteria").SequenceEqual(new List<string> { }));
 // Question 4
-Debug.Assert(AlliterationStations("./stations.txt").SequenceEqual(new List<string> {"Charing Cross, Bakerloo, Northern", "Clapham Common, Northern", "Golders Green, Northern", "Seven Sisters, Victoria", "Sloane Square, Circle, District"}));
+Debug.Assert(AlliterationStations("./stations.txt").SequenceEqual(new List<string> { "Charing Cross, Bakerloo, Northern", "Clapham Common, Northern", "Golders Green, Northern", "Seven Sisters, Victoria", "Sloane Square, Circle, District" }));
+// Question 5
+Debug.Assert(MostStations("./stations.txt") == "District");
+// Question 6
+var frequencies = GetWordFrequency("./stations.txt");
+Debug.Assert(frequencies.Take(5).SequenceEqual(new Dictionary<string, int> { ["district"] = 60, ["piccadilly"] = 54, ["central"] = 54, ["northern"] = 52, ["docklands"] = 45 }));
